b28fc56aa04129bdca88f9578cb34fb6cac75ada
Samuel Gómez <sagomadev@gmail.com>
2023-10-19 18:35:44 +0200

    Add implement function change tab option

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
 
