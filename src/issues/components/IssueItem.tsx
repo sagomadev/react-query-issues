7759b6e0e3d93255ec70b36b20e49758fddcc1ea
Samuel Gómez <sagomadev@gmail.com>
2023-10-20 14:04:43 +0200

    Set create issue at correct date

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -4,6 +4,7 @@ import { FC } from "react";
 import { useNavigate } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
 import { getIssueInfo, getIssueComments } from "../hooks/useIssue";
+import { timeSince } from "../../helpers";
 
 interface Props {
   issue: Issue;
@@ -45,7 +46,7 @@ export const IssueItem: FC<Props> = ({ issue }) => {
         <div className="d-flex flex-column flex-fill px-2">
           <span>{issue.title}</span>
           <span className="issue-subinfo">
-            #{issue.number} opened 2 days ago by{" "}
+            #{issue.number} opened {timeSince(issue.created_at)} ago by{" "}
             <span className="fw-bold">{issue.user.login}</span>
           </span>
         </div>
